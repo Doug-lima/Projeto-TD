344999d3411bcbac31373f048ffcd72ed95d5c41
Doug-lima <douglas.dasilvalima@outlook.com>
2021-09-26 22:15:24 -0300

    Menu mobile alterado

--- static/script.js
+++ static/script.js
@@ -8,5 +8,18 @@ menuMobile.addEventListener('click', () => {
     menuMobile.classList.contains("bi-list")
         ? menuMobile.classList.replace("bi-list", "bi-x")
         : menuMobile.classList.replace("bi-x", "bi-list");
-        body.classList.toggle("menu-nav-active")
+    body.classList.toggle("menu-nav-active")
 });
+
+/*Fechar o menu*/
+
+const navItem = document.querySelectorAll('.nav-item')
+
+navItem.forEach(item => {
+    item.addEventListener("click", () => {
+        if (body.classList.contains("menu-nav-active")) {
+            body.classList.remove("menu-nav-active")
+            menuMobile.classList.replace("bi-x", "bi-list");
+        }
+    })
+})
